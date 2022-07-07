2fb1a488adcfc7b462f56772b02c1b75487fb696
rajeev chkravarthy <rajeev.uba@gmail.com>
2022-07-07 20:49:29 +0530

    Update cisDemoV1.ps1

--- cisDemoV1.ps1
+++ cisDemoV1.ps1
@@ -5,7 +5,7 @@ Install-Module -Name ComputerManagementDsc
 Install-Module -Name  AuditPolicyDsc
 
 $fpath = "c:\buildActions"
-$dscfileuri = "https://urcstorage.blob.core.windows.net/imagebuilder/DemoImageConfv1.ps1?sp=r&st=2022-06-30T04:07:37Z&se=2022-06-30T12:07:37Z&spr=https&sv=2021-06-08&sr=b&sig=ek25nJ2s6s5lftOY%2Bl4kMMcm7iOKv68%2FrgxJSv5ueVU%3D"
+$dscfileuri = "https://raw.githubusercontent.com/urc1712/azureimage/main/DemoImageConfv1.ps1"
 
 Write-Host  "Azure-Image-Builder-Was-Here" | Out-File $fpath\buildActionsOutput.txt
 
